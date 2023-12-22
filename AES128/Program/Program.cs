7d17bf6249cf8ca45d2b10aaa7c3fb1eb39e34bf
Kirikulus <64729961+Kirikulus@users.noreply.github.com>
2023-12-22 13:04:21 +0300

    Add files via upload

---
+++ AES128/Program/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.IO;
+using System.Security.Cryptography;
+
+class AES128Encryption
+{
+    public static void Main()
+    {
+        string original = "Your text";
+        Console.WriteLine("Original: " + original);
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.KeySize = 128;  // Установка размера ключа в 128 бит
+            aesAlg.GenerateKey();  // Генерация ключа
+            aesAlg.GenerateIV();   // Генерация вектора инициализации
+
+            byte[] encrypted = EncryptStringToBytes_Aes(original, aesAlg.Key, aesAlg.IV);
+
+            Console.WriteLine("Encrypted: " + Convert.ToBase64String(encrypted));
+
+            string roundtrip = DecryptStringFromBytes_Aes(encrypted, aesAlg.Key, aesAlg.IV);
+            Console.WriteLine("Decrypted: " + roundtrip);
+        }
+    }
+
+    static byte[] EncryptStringToBytes_Aes(string plainText, byte[] Key, byte[] IV)
+    {
+        if (plainText == null || plainText.Length <= 0)
+            throw new ArgumentNullException("plainText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+
+        byte[] encrypted;
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msEncrypt = new MemoryStream())
+            {
+                using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                {
+                    using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                    {
+                        swEncrypt.Write(plainText);
+                    }
+                    encrypted = msEncrypt.ToArray();
+                }
+            }
+        }
+        return encrypted;
+    }
+
+    static string DecryptStringFromBytes_Aes(byte[] cipherText, byte[] Key, byte[] IV)
+    {
+        if (cipherText == null || cipherText.Length <= 0)
+            throw new ArgumentNullException("cipherText");
+        if (Key == null || Key.Length <= 0)
+            throw new ArgumentNullException("Key");
+        if (IV == null || IV.Length <= 0)
+            throw new ArgumentNullException("IV");
+
+        string plaintext = null;
+
+        using (Aes aesAlg = Aes.Create())
+        {
+            aesAlg.Key = Key;
+            aesAlg.IV = IV;
+
+            ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+            using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+            {
+                using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                {
+                    using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                    {
+                        plaintext = srDecrypt.ReadToEnd();
+                    }
+                }
+            }
+        }
+        return plaintext;
+    }
+}
